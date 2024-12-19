148f2edae5a376f0a857a4a71c2c8206b1fc049c
Aleksandar Drakaliyski <aleksandardrakaliyski@Aleksandars-MacBook-Pro.local>
2024-12-19 17:09:49 +0200

    first commit

---
+++ src/main/java/com/aleksdraka/chatwebsocket/chat/MessageType.java
@@ -0,0 +1,7 @@
+package com.aleksdraka.chatwebsocket.chat;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
---
+++ src/main/java/com/aleksdraka/chatwebsocket/config/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package com.aleksdraka.chatwebsocket.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configurable
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
+    }
+}
