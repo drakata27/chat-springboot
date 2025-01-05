7d3f06b1282d9a03a0753098ac4f9ad616a4422d
drakata27 <theall3xander@gmail.com>
2025-01-05 14:50:28 +0000

    Added dynamic rooms

--- src/main/java/com/aleksdraka/chatwebsocket/chat/ChatController.java
+++ src/main/java/com/aleksdraka/chatwebsocket/chat/ChatController.java
@@ -1,28 +1,37 @@
 package com.aleksdraka.chatwebsocket.chat;
 
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 import java.util.Objects;
 
 @Controller
 public class ChatController {
-    @MessageMapping("/chat.sendMessage")
-    @SendTo("/topic/public")
-    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
-        return chatMessage;
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public ChatController(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
     }
 
-    @MessageMapping("/chat.addUser")
-    @SendTo("/topic/public")
-    public ChatMessage addUser(
+    @MessageMapping("/chat.sendMessage/{roomId}")
+    public void sendMessage(
             @Payload ChatMessage chatMessage,
-            SimpMessageHeaderAccessor headerAccessor
+            @DestinationVariable String roomId) {
+
+        messagingTemplate.convertAndSend("/topic/" + roomId, chatMessage);
+    }
+
+    @MessageMapping("/chat.addUser/{roomId}")
+    public void addUser(
+            @Payload ChatMessage chatMessage,
+            SimpMessageHeaderAccessor headerAccessor,
+            @DestinationVariable String roomId
     ) {
         Objects.requireNonNull(headerAccessor.getSessionAttributes()).put("username", chatMessage.getSender());
-        return chatMessage;
+        messagingTemplate.convertAndSend("/topic/" + roomId, chatMessage);
     }
 }
